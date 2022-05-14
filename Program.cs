1586c42d5339e9316733f069bbe6d7e64232f730
Alexey Grebenyuk <vladimir-g2010@mail.ru>
2022-05-14 22:07:28 +0900

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+using System;
+
+namespace test_MVS_github 
+{
+    internal class Hello_GitHub
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello GitHub!");
+        }
+    }
+}
